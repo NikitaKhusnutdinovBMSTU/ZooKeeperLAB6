ba180ae5c74dadbb8470f31cf5b80468aae73157
x1Dman <glaider102@mail.ru>
2019-11-18 16:19:53 +0300

    Scripted auto-commit on change (2019-11-18 16:19:53) by gitwatch.sh

--- src/main/java/bmstu/zookeeper/httpAnonymizer.java
+++ src/main/java/bmstu/zookeeper/httpAnonymizer.java
@@ -1,10 +1,18 @@
 package bmstu.zookeeper;
 
 
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
 public class httpAnonymizer {
 
-    public void main(String[] args) {
-
-        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 2000, this);
+    public void main(String[] args) throws IOException {
+        Watcher watcher = event -> {
+            
+        };
+        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 2000, watcher);
     }
 }
