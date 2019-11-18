1944f3e2d44df726d674fb7444cc147b6b695ce7
x1Dman <glaider102@mail.ru>
2019-11-19 02:36:15 +0300

    Scripted auto-commit on change (2019-11-19 02:36:15) by gitwatch.sh

--- src/main/java/bmstu/zookeeper/storageActor.java
+++ src/main/java/bmstu/zookeeper/storageActor.java
@@ -3,7 +3,7 @@ package bmstu.zookeeper;
 import akka.actor.AbstractActor;
 
 public class storageActor extends AbstractActor {
-    
+
     @Override
     public Receive createReceive() {
         return null;
