5f6945122f79a3a9c2daa9abd7aa0ab820e1eac9
x1Dman <glaider102@mail.ru>
2019-11-18 16:25:09 +0300

    Scripted auto-commit on change (2019-11-18 16:25:09) by gitwatch.sh

--- src/main/java/bmstu/zookeeper/httpAnonymizer.java
+++ src/main/java/bmstu/zookeeper/httpAnonymizer.java
@@ -6,9 +6,15 @@ import org.apache.zookeeper.*;
 import java.io.IOException;
 
 public class httpAnonymizer {
+    private final static String SERVER_NUMBER_1 = "127.0.0.1:2181";
+    private final static String SERVER_NUMBER_2 = "127.0.0.1:2182";
+    private final static String SERVER_NUMBER_3 = "127.0.0.1:2183";
+    private final static String SERVER_NUMBER_4 = "127.0.0.1:2184";
+    private final static String SERVER_NUMBER_5 = "127.0.0.1:2185";
+
 
     public void main(String[] args) throws IOException, KeeperException, InterruptedException {
-        
+
         ZooKeeper zoo = new ZooKeeper(
                 "127.0.0.1:2181",
                 2000,
