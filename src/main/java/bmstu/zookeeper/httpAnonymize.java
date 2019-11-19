df0916cf9eb08c950a2d4dc52928a7f8591f3275
x1Dman <glaider102@mail.ru>
2019-11-19 16:14:57 +0300

    Scripted auto-commit on change (2019-11-19 16:14:57) by gitwatch.sh

--- src/main/java/bmstu/zookeeper/httpAnonymize.java
+++ src/main/java/bmstu/zookeeper/httpAnonymize.java
@@ -6,7 +6,7 @@ import org.apache.zookeeper.*;
 import java.io.IOException;
 import java.util.List;
 
-public class httpAnonymizer {
+public class httpAnonymize {
     private final static String SERVER_NUMBER_1 = "127.0.0.1:2181";
     private final static String SERVER_NUMBER_2 = "127.0.0.1:2182";
     private final static String SERVER_NUMBER_3 = "127.0.0.1:2183";
@@ -24,17 +24,13 @@ public class httpAnonymizer {
                 }
         );
 
-//        zoo.create(
-//                "/servers",
-//                "data".getBytes(),
-//                ZooDefs.Ids.OPEN_ACL_UNSAFE,
-//                CreateMode.PERSISTENT
-//        );
-        List<String> servers = zoo.getChildren("/servers", a->{});
-        for(String s : servers){
-            byte[] data = zoo.getData("/servers/" + s,false, null);
-            System.out.println("/servers/" + s + " data = " + new String(data));
+        while(true){
+            
+            List<String> servers = zoo.getChildren("/servers", a->{});
+            for(String s : servers){
+                byte[] data = zoo.getData("/servers/" + s,false, null);
+                System.out.println("/servers/" + s + " data = " + new String(data));
+            }
         }
-        //zoo.delete("/s", 4);
     }
 }
